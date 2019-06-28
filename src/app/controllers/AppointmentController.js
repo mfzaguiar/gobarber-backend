bbf0cff706029513e1fb116f6afb9aa4bcd40101
mfzaguiar <first_mf@hotmail.com>
2019-06-28 18:31:50 -0300

    Add marcar notificações como lidas e cancelamento de agendamentos

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 
 import User from '../models/User';
@@ -87,6 +87,12 @@ class AppointmentController {
         .json({ error: 'Apointment date is not available' });
     }
 
+    if (req.userId === provider_id) {
+      return res
+        .status(400)
+        .json({ error: 'Cant create appointment with yourself' });
+    }
+
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
@@ -107,6 +113,29 @@ class AppointmentController {
       content: `Novo agendamento de ${user.name} para ${formattedDate}.`,
       user: provider_id,
     });
+
+    return res.json(appointment);
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id);
+
+    if (appointment.user_id !== req.userId) {
+      return res.status(401).json({
+        error: "You don't have permission to cancel this appointment.",
+      });
+    }
+
+    const dateWithSub = subHours(appointment.date, 2);
+
+    if (isBefore(dateWithSub, new Date())) {
+      return res.status(401).json({
+        error: 'You can only cancel appointments two hours in advance.',
+      });
+    }
+
+    appointment.canceled_at = new Date();
+    await appointment.save();
 
     return res.json(appointment);
   }
