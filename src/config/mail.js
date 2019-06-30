65e7fc5a12a3ef1f4524506fbeb01f882c378cd5
mfzaguiar <first_mf@hotmail.com>
2019-06-30 16:35:50 -0300

    Add configurações nodemailer e templates de email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'b419fe6b1c85c4',
+    pass: 'ec37b4eebaa84e',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
