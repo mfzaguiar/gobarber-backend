7aed0df0fb646ccb80d81f359f3cd89af6740fab
mfzaguiar <first_mf@hotmail.com>
2019-07-01 16:27:56 -0300

    Config tratamento de exceções com sentry e configuração variáveis de ambiente.

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'b419fe6b1c85c4',
-    pass: 'ec37b4eebaa84e',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
